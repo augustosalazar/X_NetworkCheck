d276187067eba7d26d838e2b7861e1386a97768b
augustosalazar <augustosalazar@uninorte.edu.co>
2017-11-14 16:53:36 -0500

    * .gitignore: Check ok
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * X_NetworkCheck.sln:
    * packages.config:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * App.xaml:
    * App.xaml.cs:
    * LaunchScreen.storyboard:
    * AboutAssets.txt:
    * X_NetworkCheck.iOS.csproj:
    * packages.config:
    * AssemblyInfo.cs:
    * styles.xml:
    * X_NetworkCheck.Droid.csproj:
    * Contents.json:
    * icon.png:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * Resource.designer.cs:
    * X_NetworkCheck.csproj:
    * AndroidManifest.xml:
    * X_NetworkCheckPage.xaml:
    * icon.png:
    * icon.png:
    * icon.png:
    * AssemblyInfo.cs:
    * X_NetworkCheckPage.xaml.cs:
    * Contents.json:

---
+++ X_NetworkCheck/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+namespace X_NetworkCheck
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new X_NetworkCheckPage());
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ X_NetworkCheck/X_NetworkCheckPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+namespace X_NetworkCheck
+{
+    public partial class X_NetworkCheckPage : ContentPage
+    {
+        public X_NetworkCheckPage()
+        {
+            //Add the Nuget package Xam.Plugin.Connectivity  
+            InitializeComponent();
+            checkConnectivity(CrossConnectivity.Current.IsConnected);
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+        }
+
+
+
+        async void HandleConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            await DisplayAlert("HandleConnectivityChanged","Changed","Ok");
+            checkConnectivity(e.IsConnected);
+        }
+
+        public void checkConnectivity(bool sw){
+            if (sw)
+            {
+                if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+                {
+                    var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                    webStatusLabel.Text = connectionType.ToString();
+                }
+            }
+            else
+            {
+                webStatusLabel.Text = "Offline";
+            }
+        }
+    }
+}
